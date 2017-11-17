ea4e53bcdcadda217a35b049b10d499b11ccbf99
Jared Bondurant <31866081+zrasco@users.noreply.github.com>
2017-11-16 16:10:22 -0800

    Added framework for subscription system. Same concept as Zeb's achievement system.

--- TaskItSite/TaskItSite/Data/ApplicationDbContext.cs
+++ TaskItSite/TaskItSite/Data/ApplicationDbContext.cs
@@ -24,9 +24,13 @@ namespace TaskItSite.Data
         public DbSet<AchievementCategory> AchievementCategories { get; set; }
         public DbSet<Reminder> Reminders { get; set; }
         public DbSet<Models.Task> Tasks { get; set; }
+        public DbSet<Subscription> Subscriptions { get; set; }
+        public DbSet<GlobalSubscription> GlobalSubscriptions { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<GlobalSubscription>().ToTable("GlobalSubscriptions");
+            modelBuilder.Entity<Subscription>().ToTable("Subscriptions");
             modelBuilder.Entity<UserAchievement>().ToTable("UserAchievements");
             modelBuilder.Entity<GlobalAchievement>().ToTable("GlobalAchievements");
             modelBuilder.Entity<AchievementCategory>().ToTable("AchievementCategories");
---
+++ TaskItSite/TaskItSite/Models/MainViewModels/SubscriptionsViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskItSite.Models.MainViewModels
+{
+    public class SubscriptionWrapper
+    {
+        public GlobalSubscription Subscription { get; set; }
+        public bool IsSubscribed { get; set; }
+    }
+
+    public class SubscriptionsViewModel
+    {
+        public ApplicationUser CurrentUser { get; set; }
+        public List<SubscriptionWrapper> SubscriptionWrapperList { get; set; }
+
+        public string StatusMessage { get; set; }
+    }
+}
