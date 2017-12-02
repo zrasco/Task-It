786b24d8c35342d7dba7e88bb75ec445528575de
Pedro Turcios <31866081+zrasco@users.noreply.github.com>
2017-12-01 20:14:49 -0800

    Merged list page into Task Manager page, reorganized javascript for cleaner code

--- TaskItSite/TaskItSite/wwwroot/js/taskItCalendar.js
+++ TaskItSite/TaskItSite/wwwroot/js/taskItCalendar.js
@@ -1,10 +1,10 @@
 ﻿/**
- * TaskIt Calendar application
- * Exclusively the current user's calendar
- */
+        * TaskIt Calendar application
+        * Exclusively the current user's calendar
+        */
 var calendarDemoApp = angular.module('TaskItApp', ['ui.calendar', 'ui.bootstrap']);
 
-//controller to pull information from the database 
+//controller to pull information from the database
 calendarDemoApp.controller('CalendarCtrl', ['$scope', '$http', 'uiCalendarConfig', function ($scope, $http, uiCalendarConfig) {
 
     //selected calendar
@@ -13,23 +13,25 @@ calendarDemoApp.controller('CalendarCtrl', ['$scope', '$http', 'uiCalendarConfig
 
     //list of tasks
     $scope.tasks = [];
-    $scope.tasksSources = [$scope.tasks];
+
     //list of presented tasks via calendar (events)
     $scope.events = [];
 
     //Load events from server
-    $scope.tasks = @Html.Raw(Json.Serialize(Model));
+    $scope.tasks = personalRawData;
+    console.log("in calendar", $scope.tasks);
 
     //go through each event and set calendar location for each
     $scope.events.slice(0, $scope.tasks.length);
-    angular.forEach(data.data, function (value) {
+    //push events onto the actual calendar
+    angular.forEach($scope.tasks, function (value) {
         $scope.events.push({
-            title: value.title,
+            title: value.id,
             description: value.description,
-            start: value.createdDate,
-            end: value.dueDate,
-            allDay: value.isActive
+            start: new Date(value.dueDate),
+            allDay: true
         });
+        console.log(new Date(value.createdDate));
     });
 
     //configure calendar
@@ -38,45 +40,21 @@ calendarDemoApp.controller('CalendarCtrl', ['$scope', '$http', 'uiCalendarConfig
         calendar: {
             height: 450,
             editable: true,
+            displayEventTime: true,
             header: {
                 left: 'title',
                 center: '',
                 right: 'today prev,next'
             },
             //scope events
-            eventClick: $scope.alertOnEventClick,
+            eventClick: function (event) {
+                $scope.SelectedEvent = event;
+            },
             eventDrop: $scope.alertOnDrop,
             eventResize: $scope.alertOnResize,
             eventRender: $scope.eventRender
         }
     };
-
-    /*Add event button*/
-    $scope.addEvent = function () {
-        //get event info form user
-        //false data for now
-        var today = new Date()
-        var CreatedDate = today.getFullYear() + '-' + today.getMonth() + '-' + today.getDay()
-        CreatedDate += ' ' + today.getHours() + ':' + today.getMinutes() + '-' + today.getSeconds()
-        var DueDate = '2017-11-20 19:00:00'
-        var Summary = "This is a summary"
-        var Description = "This is a description"
-        var isPin = false
-
-        //get User Id
-        
-        //call as JSON object
-        var JSONObj = {
-            CreatedDate: CreatedDate,
-            DueDate: DueDate,
-            Summary: Summary,
-            Description: Description,
-            IsPin: isPin,
-            UserId: $scope.userId
-        }
-        
-    }
-
-    /*Edit button (will be merged with add later on)*/
+    /*push data to calendar*/
+    $scope.eventSources = [$scope.events];
 }])
-
